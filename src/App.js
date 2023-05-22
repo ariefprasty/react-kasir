1feec40d9d7847a1b95509f7853daa52c2fafbb2
ariefprasty <130353584+ariefprasty@users.noreply.github.com>
2023-05-22 22:30:18 +0800

    adding menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import { Col, Container, Row } from "react-bootstrap";
-import "./App.css";
-import { Hasil, ListCatagories, NavbarComponents } from "./Components/Index";
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponents />
-      <div className="mt-3">
-        <Container fluid>
-          <Row>
-            <ListCatagories />
-            <Col>
-              <h4>Daftar Produk</h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
-      </div>
-    </div>
-  );
-}
+import {
+  Hasil,
+  ListCatagories,
+  Menus,
+  NavbarComponents,
+} from "./Components/Index";
+import React, { Component } from "react";
+import { API_URL } from "./Utils/Constans";
+import axios from "axios";
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(API_URL + "products")
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    const { menus } = this.state;
+    return (
+      <div className="App">
+        <NavbarComponents />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCatagories />
+              <Col>
+                <h4>Daftar Produk</h4>
+                <hr />
+                <Row>
+                  {menus &&
+                    menus.map((menu) => <Menus key={menu.id} menu={menu} />)}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,5 +1,6 @@
 import Hasil from "./Hasil";
 import ListCatagories from "./ListCatagories";
 import NavbarComponents from "./NavbarComponents";
+import Menus from "./menus";
 
-export { Hasil, ListCatagories, NavbarComponents };
+export { Hasil, ListCatagories, NavbarComponents, Menus };
